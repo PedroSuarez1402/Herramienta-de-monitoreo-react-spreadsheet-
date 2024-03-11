c136ba948ab9fb6314d540b20c937eab585a9549
PedroSuarez1402 <pssuarez1402@gmail.com>
2024-03-11 10:30:35 -0500

    second commit

--- src/components/dashboard/LineChart.js
+++ src/components/dashboard/LineChart.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import FusionCharts from 'react-fusioncharts';
-import FusionChartsComponent from 'react-fusioncharts';
-import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 import { LineChartConfig } from './chartConfigs'; // Configuración de la gráfica de línea
 
 // Aplicar tema fusion
-FusionChartsComponent.fcRoot(FusionCharts, FusionTheme);
-
 const LineChart = ({ data }) => {
+    // Aplicar tema directamente a la configuración del gráfico
+    const chartConfigWithTheme = {
+        ...LineChartConfig,
+        chart: {
+            ...LineChartConfig.chart,
+            theme: 'fusion',
+        },
+    };
+
     const chartData = {
-    chart: LineChartConfig,
-    data: data.map((value, index) => ({ label: `Data ${index + 1}`, value }))
+        ...chartConfigWithTheme,
+        data: data.map((value, index) => ({ label: `Data ${index + 1}`, value }))
     };
 
     return (
-    <div>
-        <h2>Gráfico de Línea</h2>
-        <FusionCharts {...chartData} />
-    </div>
+        <div>
+            <h2>Gráfico de Línea</h2>
+            <FusionCharts {...chartData} />
+        </div>
     );
 };
-
 export default LineChart;
---
+++ src/components/dashboard/chartConfigs.js
@@ -0,0 +1,15 @@
+export const LineChartConfig = {
+    type: 'line',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+        chart: {
+        caption: 'Datos del Router',
+        xAxisName: 'Tiempo',
+        yAxisName: 'Valor',
+        theme: 'fusion',
+        },
+        data: [],
+    },
+};
